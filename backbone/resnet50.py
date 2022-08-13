ee2b3b053eee1646961ba6fa931351f9721aacc5
绿色羽毛 <lvseyumao@foxmail.com>
2022-08-13 15:07:47 +0800

    Add files via upload

---
+++ backbone/resnet50.py
@@ -0,0 +1,17 @@
+#  !/usr/bin/env  python
+#  -*- coding:utf-8 -*-
+# @Time     : 2022.04
+# @Author   : 绿色羽毛
+# @Email    : lvseyumao@foxmail.com
+# @Blog     : https://blog.csdn.net/ViatorSun
+# @Paper    : 
+# @arXiv    : 
+# @version  : "1.0" 
+# @Note     : 
+# 
+#
+
+
+import torchvision.models as models
+
+model = models.resnet34()
